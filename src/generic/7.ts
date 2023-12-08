53bff0bb693f091a5a9a977dde3e9ed0677aef37
Chalaya22 <chalayaolga22@gmail.com>
2023-12-08 18:15:32 +0200

    comit 3

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -4,16 +4,17 @@
 */
 
 export enum UserRole {
-  admin = 'admin',
-  editor = 'editor',
-  guest = 'guest',
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
 }
-
+type RoleDescriptionType = Record<string, string>;
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+const RoleDescription: RoleDescriptionType = {
+  admin: "Admin User",
+  editor: "Editor User",
+  guest: "Guest User",
 };
 
 export {};
+// const RoleDescription: Record<UserRole, string>;
